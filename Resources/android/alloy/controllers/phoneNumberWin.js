5f09200a4f419f15b41bc345533ff446223472f2
Ahmad Amin <ahmad.amin.ma@gmail.com>
2014-02-13 00:52:56 +0200

    Revert 439393b..7673ebe
    This rolls back to commit 439393b5bc1ce73a4b717e3306031d73c1d2c3d1.

--- Resources/android/alloy/controllers/phoneNumberWin.js
+++ Resources/android/alloy/controllers/phoneNumberWin.js
@@ -1,22 +1,33 @@
 function Controller() {
-    function openPicker() {
-        false == $.picker.visible && ($.picker.visible = true);
-        animation.fadeIn($.picker, 500);
+    function openOrClosePicker() {
+        $.txt_country_ios.blur();
+        "false" == $.picker.pickerview.visible && ($.picker.pickerview.visible = true);
+        if (pickerAlive) {
+            animation.fadeOut($.picker.pickerview, 200);
+            pickerAlive = false;
+        } else {
+            animation.popIn($.picker.pickerview);
+            pickerAlive = true;
+        }
     }
     function changeCountryAndCountryCode() {
-        currentCountryCode = $.picker.getSelectedRow(0).id.toLowerCase();
+        currentCountryCode = $.picker.picker.getSelectedRow(0).id.toLowerCase();
+        $.lbl_countryCode.text = "+" + countries[currentCountryCode].phoneCode;
     }
     function continueBtnPressed() {
-        "0" == $.txt_phoneNumber.value.charAt(0) && alert("fuck me");
         var phoneNumberRegex = /^[0-9]{9,15}$/;
         if ($.txt_phoneNumber.value.match(phoneNumberRegex)) {
-            $.dialog_confirm.message = "Do do you confirm that this is your number: +" + allCountries[currentCountryCode].phoneCode + parseInt($.txt_phoneNumber.value, 10) + "\nAn SMS with your access code will be sent to this number.";
+            $.dialog_confirm.message = "Do do you confirm that this is your number: " + $.lbl_countryCode.text + $.txt_phoneNumber.value + "\nAn SMS with your access code will be sent to this number.";
             $.dialog_confirm.show();
         } else alert("The number you entered is not valid");
     }
+    function openWebView(e) {
+        var webview = Alloy.createController("webview", e.source.url).getView();
+        webview.open();
+    }
     function dialogConfirmPressed(e) {
         if (0 == e.index) $.txt_phoneNumber.focus(); else {
-            Alloy.Globals.globalUserSignUpData.phone = "+" + allCountries[currentCountryCode].phoneCode + parseInt($.txt_phoneNumber.value, 10);
+            Alloy.Globals.globalUserSignUpData.phone = $.lbl_countryCode.text + $.txt_phoneNumber.value;
             var smsWin = Alloy.createController("smsWin").getView();
             smsWin.open();
         }
@@ -32,76 +43,139 @@ function Controller() {
     $.__views.win = Ti.UI.createWindow({
         backgroundColor: "white",
         id: "win",
-        title: "Bofff Me Setup"
+        title: "Your Phone Number"
     });
     $.__views.win && $.addTopLevelView($.__views.win);
-    $.__views.__alloyId172 = Ti.UI.createScrollView({
+    $.__views.view_1 = Ti.UI.createScrollView({
         height: Ti.UI.FILL,
-        top: 40,
-        scrollingEnabled: "false",
+        id: "view_1",
         layout: "vertical",
+        scrollingEnabled: "false"
+    });
+    $.__views.win.add($.__views.view_1);
+    $.__views.__alloyId172 = Ti.UI.createLabel({
+        font: {
+            fontSize: "14dp",
+            fontWeight: "bold"
+        },
+        color: "black",
+        top: "2%",
+        left: 20,
+        width: "75%",
+        height: Ti.UI.SIZE,
+        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+        keyboardType: Ti.UI.KEYBOARD_PHONE_PAD,
+        text: "Choose Your Country",
         id: "__alloyId172"
     });
-    $.__views.win.add($.__views.__alloyId172);
-    $.__views.__alloyId173 = Ti.UI.createLabel({
-        bottom: 40,
-        width: "80%",
-        textAlign: "center",
-        color: "#2279bc",
+    $.__views.view_1.add($.__views.__alloyId172);
+    $.__views.picker = Alloy.createController("picker", {
+        id: "picker",
+        top: "10",
+        __parentSymbol: $.__views.view_1
+    });
+    $.__views.picker.setParent($.__views.view_1);
+    $.__views.lbl_typeYourPhone = Ti.UI.createLabel({
         font: {
-            fontSize: "17"
+            fontSize: "14dp",
+            fontWeight: "bold"
         },
-        text: "Keep your bofff friends in sync with your contact list by registering your phone number.",
+        color: "black",
+        top: "2%",
+        left: 20,
+        width: "75%",
+        height: Ti.UI.SIZE,
+        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+        keyboardType: Ti.UI.KEYBOARD_PHONE_PAD,
+        text: "Type your phone number",
+        id: "lbl_typeYourPhone"
+    });
+    $.__views.view_1.add($.__views.lbl_typeYourPhone);
+    $.__views.view_2 = Ti.UI.createView({
+        height: Ti.UI.SIZE,
+        id: "view_2",
+        layout: "horizontal",
+        horizontalWrap: "true"
+    });
+    $.__views.view_1.add($.__views.view_2);
+    $.__views.lbl_countryCode = Ti.UI.createLabel({
+        font: {
+            fontSize: "14dp",
+            fontWeight: "bold"
+        },
+        color: "black",
+        top: "2%",
+        left: 20,
+        width: "75%",
+        height: Ti.UI.SIZE,
+        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+        keyboardType: Ti.UI.KEYBOARD_PHONE_PAD,
+        text: "+966",
+        id: "lbl_countryCode"
+    });
+    $.__views.view_2.add($.__views.lbl_countryCode);
+    $.__views.txt_phoneNumber = Ti.UI.createTextField({
+        font: {
+            fontSize: "14dp"
+        },
+        top: "2%",
+        left: 20,
+        width: "75%",
+        height: Ti.UI.SIZE,
+        color: "black",
+        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+        keyboardType: Ti.UI.KEYBOARD_PHONE_PAD,
+        id: "txt_phoneNumber"
+    });
+    $.__views.view_2.add($.__views.txt_phoneNumber);
+    $.__views.__alloyId173 = Ti.UI.createLabel({
+        font: {
+            fontSize: "14dp",
+            fontWeight: "bold"
+        },
+        color: "black",
+        top: "2%",
+        left: 20,
+        width: "75%",
+        height: Ti.UI.SIZE,
+        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+        keyboardType: Ti.UI.KEYBOARD_PHONE_PAD,
+        text: "Why my phone number ?",
+        url: "http://www.facebook.com",
         id: "__alloyId173"
     });
-    $.__views.__alloyId172.add($.__views.__alloyId173);
-    $.__views.picker = Ti.UI.createPicker({
-        selectionIndicator: true,
-        height: "42",
-        bottom: 10,
-        width: "80%",
-        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
-        keyboardType: Ti.UI.KEYBOARD_PHONE_PAD,
+    $.__views.view_1.add($.__views.__alloyId173);
+    openWebView ? $.__views.__alloyId173.addEventListener("click", openWebView) : __defers["$.__views.__alloyId173!click!openWebView"] = true;
+    $.__views.lbl_alreadyRegistered = Ti.UI.createLabel({
         font: {
-            fontSize: "14.5"
+            fontSize: "14dp",
+            fontWeight: "bold"
         },
         color: "black",
-        bubbleParent: false,
-        id: "picker"
+        text: "Already Registered ?",
+        id: "lbl_alreadyRegistered",
+        url: "http://www.google.com"
     });
-    $.__views.__alloyId172.add($.__views.picker);
-    changeCountryAndCountryCode ? $.__views.picker.addEventListener("change", changeCountryAndCountryCode) : __defers["$.__views.picker!change!changeCountryAndCountryCode"] = true;
-    $.__views.txt_phoneNumber = Ti.UI.createTextField({
-        softKeyboardOnFocus: Ti.UI.Android.SOFT_KEYBOARD_SHOW_ON_FOCUS,
-        height: "42",
-        bottom: 10,
-        width: "80%",
-        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
-        keyboardType: Ti.UI.KEYBOARD_PHONE_PAD,
-        font: {
-            fontSize: "14.5"
-        },
-        color: "black",
-        bubbleParent: false,
-        id: "txt_phoneNumber",
-        hintText: "Phone Number"
-    });
-    $.__views.__alloyId172.add($.__views.txt_phoneNumber);
+    $.__views.view_1.add($.__views.lbl_alreadyRegistered);
+    openWebView ? $.__views.lbl_alreadyRegistered.addEventListener("click", openWebView) : __defers["$.__views.lbl_alreadyRegistered!click!openWebView"] = true;
     $.__views.__alloyId174 = Ti.UI.createButton({
-        top: 20,
-        color: "white",
+        color: "black",
         backgroundColor: "#2279bc",
         borderRadius: 5,
         font: {
-            fontSize: "16",
+            fontSize: "15dp",
             fontWeight: "bold"
         },
-        height: "40",
-        width: "50%",
+        height: Ti.UI.SIZE,
+        width: "75%",
+        top: "2%",
+        left: 20,
+        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+        keyboardType: Ti.UI.KEYBOARD_PHONE_PAD,
         title: "Continue",
         id: "__alloyId174"
     });
-    $.__views.__alloyId172.add($.__views.__alloyId174);
+    $.__views.view_1.add($.__views.__alloyId174);
     continueBtnPressed ? $.__views.__alloyId174.addEventListener("click", continueBtnPressed) : __defers["$.__views.__alloyId174!click!continueBtnPressed"] = true;
     var __alloyId176 = [];
     __alloyId176.push("Edit");
@@ -116,32 +190,34 @@ function Controller() {
     _.extend($, $.__views);
     var currentCountryCode = Alloy.Globals.location;
     currentCountryCode = currentCountryCode.toLowerCase();
-    var allCountries = require("countries");
+    var countries = require("countries");
     var selectedRow, pickerRows = [], counter = 0;
-    for (var country in allCountries) {
+    for (var country in countries) {
         var row = Titanium.UI.createPickerRow({
-            title: allCountries[country].name + " (+" + allCountries[country].phoneCode + ")",
+            title: countries[country].name,
             id: country.toUpperCase()
         });
-        allCountries[currentCountryCode] == allCountries[country] && (selectedRow = counter);
+        countries[currentCountryCode] == countries[country] && (selectedRow = counter);
         counter++;
         pickerRows.push(row);
     }
-    $.picker.add(pickerRows);
-    $.picker.setSelectedRow(0, selectedRow, true);
+    $.picker.picker.add(pickerRows);
+    $.picker.picker.setSelectedRow(0, selectedRow, true);
+    $.lbl_countryCode.text = "+" + countries[currentCountryCode].phoneCode;
     var animation;
-    $.win.addEventListener("click", function() {
-        $.txt_phoneNumber.blur();
-    });
+    var pickerAlive;
+    $.addClass($.picker.picker, "allStyles");
+    $.addClass($.picker.picker, "androidPicker");
+    $.picker.picker.addEventListener("change", changeCountryAndCountryCode);
     $.win.addEventListener("androidback", function() {
         $.win.close({
             activityExitAnimation: Ti.Android.R.anim.slide_out_right
         });
     });
-    __defers["$.__views.lbl_country_ios!click!openPicker"] && $.__views.lbl_country_ios.addEventListener("click", openPicker);
-    __defers["$.__views.picker!change!changeCountryAndCountryCode"] && $.__views.picker.addEventListener("change", changeCountryAndCountryCode);
+    __defers["$.__views.txt_country_ios!focus!openOrClosePicker"] && $.__views.txt_country_ios.addEventListener("focus", openOrClosePicker);
+    __defers["$.__views.__alloyId173!click!openWebView"] && $.__views.__alloyId173.addEventListener("click", openWebView);
+    __defers["$.__views.lbl_alreadyRegistered!click!openWebView"] && $.__views.lbl_alreadyRegistered.addEventListener("click", openWebView);
     __defers["$.__views.__alloyId174!click!continueBtnPressed"] && $.__views.__alloyId174.addEventListener("click", continueBtnPressed);
-    __defers["$.__views.picker!change!changeCountryAndCountryCode"] && $.__views.picker.addEventListener("change", changeCountryAndCountryCode);
     __defers["$.__views.dialog_confirm!click!dialogConfirmPressed"] && $.__views.dialog_confirm.addEventListener("click", dialogConfirmPressed);
     _.extend($, exports);
 }
--- Resources/android/alloy/controllers/welcomeWin.js
+++ Resources/android/alloy/controllers/welcomeWin.js
@@ -37,14 +37,15 @@ function Controller() {
                 offset: .8
             } ]
         },
-        id: "win"
+        id: "win",
+        title: "Welcome to Bofff Me !"
     });
     $.__views.win && $.addTopLevelView($.__views.win);
-    $.__views.__alloyId205 = Ti.UI.createView({
-        id: "__alloyId205"
+    $.__views.viewandroid = Ti.UI.createView({
+        id: "viewandroid"
     });
-    $.__views.win.add($.__views.__alloyId205);
-    $.__views.__alloyId206 = Ti.UI.createLabel({
+    $.__views.win.add($.__views.viewandroid);
+    $.__views.__alloyId205 = Ti.UI.createLabel({
         left: 10,
         top: "5%",
         width: Ti.UI.SIZE,
@@ -54,18 +55,18 @@ function Controller() {
             fontSize: "18"
         },
         text: "WELCOME TO BOFFF ME!",
-        id: "__alloyId206"
+        id: "__alloyId205"
     });
-    $.__views.__alloyId205.add($.__views.__alloyId206);
-    $.__views.__alloyId207 = Ti.UI.createImageView({
-        image: "/images/bofffme_logo_512x512.png",
+    $.__views.viewandroid.add($.__views.__alloyId205);
+    $.__views.logo = Ti.UI.createImageView({
+        image: "/images/bofffme_logo.png",
         width: "200",
         height: "200",
         top: "25%",
-        id: "__alloyId207"
+        id: "logo"
     });
-    $.__views.__alloyId205.add($.__views.__alloyId207);
-    $.__views.__alloyId208 = Ti.UI.createButton({
+    $.__views.viewandroid.add($.__views.logo);
+    $.__views.__alloyId206 = Ti.UI.createButton({
         bottom: 0,
         font: {
             fontSize: "22",
@@ -76,15 +77,15 @@ function Controller() {
         backgroundColor: "transparent",
         color: "white",
         title: "Continue",
-        id: "__alloyId208"
+        id: "__alloyId206"
     });
-    $.__views.__alloyId205.add($.__views.__alloyId208);
-    openPhoneNumberWin ? $.__views.__alloyId208.addEventListener("click", openPhoneNumberWin) : __defers["$.__views.__alloyId208!click!openPhoneNumberWin"] = true;
+    $.__views.viewandroid.add($.__views.__alloyId206);
+    openPhoneNumberWin ? $.__views.__alloyId206.addEventListener("click", openPhoneNumberWin) : __defers["$.__views.__alloyId206!click!openPhoneNumberWin"] = true;
     exports.destroy = function() {};
     _.extend($, $.__views);
     $.win.navBarHidden = true;
     $.win.open();
-    __defers["$.__views.__alloyId208!click!openPhoneNumberWin"] && $.__views.__alloyId208.addEventListener("click", openPhoneNumberWin);
+    __defers["$.__views.__alloyId206!click!openPhoneNumberWin"] && $.__views.__alloyId206.addEventListener("click", openPhoneNumberWin);
     _.extend($, exports);
 }
 
