7673ebe44655d70d2dfac4b7933b402b2474b2cd
Ahmed Atif <ahmed.atif15@gmail.com>
2014-02-13 00:49:23 +0200

    Enhancements on phoneNumberWin.js
    Enhancements include:
     - Final version of UI
     - Deleting picker.xml, it has no longer use
     - Adding the picker code in the xml file instead of picker.xml
     - Deleting a lot of not useful code for android and iOS
     - Adding description text
     - Removing the toolbar for iOS picker and keyboard
     - Handling if the phone number has a zero on the left
    
    Tasks Remaining in phoneNumberWin:
     - Handling iOS label’s bug in bubbleParent
     - Choosing the best animation for picker
     - Using IP address instead of location to get the country code

--- Resources/android/alloy/controllers/welcomeWin.js
+++ Resources/android/alloy/controllers/welcomeWin.js
@@ -37,15 +37,14 @@ function Controller() {
                 offset: .8
             } ]
         },
-        id: "win",
-        title: "Welcome to Bofff Me !"
+        id: "win"
     });
     $.__views.win && $.addTopLevelView($.__views.win);
-    $.__views.viewandroid = Ti.UI.createView({
-        id: "viewandroid"
+    $.__views.__alloyId205 = Ti.UI.createView({
+        id: "__alloyId205"
     });
-    $.__views.win.add($.__views.viewandroid);
-    $.__views.__alloyId205 = Ti.UI.createLabel({
+    $.__views.win.add($.__views.__alloyId205);
+    $.__views.__alloyId206 = Ti.UI.createLabel({
         left: 10,
         top: "5%",
         width: Ti.UI.SIZE,
@@ -55,18 +54,18 @@ function Controller() {
             fontSize: "18"
         },
         text: "WELCOME TO BOFFF ME!",
-        id: "__alloyId205"
+        id: "__alloyId206"
     });
-    $.__views.viewandroid.add($.__views.__alloyId205);
-    $.__views.logo = Ti.UI.createImageView({
-        image: "/images/bofffme_logo.png",
+    $.__views.__alloyId205.add($.__views.__alloyId206);
+    $.__views.__alloyId207 = Ti.UI.createImageView({
+        image: "/images/bofffme_logo_512x512.png",
         width: "200",
         height: "200",
         top: "25%",
-        id: "logo"
+        id: "__alloyId207"
     });
-    $.__views.viewandroid.add($.__views.logo);
-    $.__views.__alloyId206 = Ti.UI.createButton({
+    $.__views.__alloyId205.add($.__views.__alloyId207);
+    $.__views.__alloyId208 = Ti.UI.createButton({
         bottom: 0,
         font: {
             fontSize: "22",
@@ -77,15 +76,15 @@ function Controller() {
         backgroundColor: "transparent",
         color: "white",
         title: "Continue",
-        id: "__alloyId206"
+        id: "__alloyId208"
     });
-    $.__views.viewandroid.add($.__views.__alloyId206);
-    openPhoneNumberWin ? $.__views.__alloyId206.addEventListener("click", openPhoneNumberWin) : __defers["$.__views.__alloyId206!click!openPhoneNumberWin"] = true;
+    $.__views.__alloyId205.add($.__views.__alloyId208);
+    openPhoneNumberWin ? $.__views.__alloyId208.addEventListener("click", openPhoneNumberWin) : __defers["$.__views.__alloyId208!click!openPhoneNumberWin"] = true;
     exports.destroy = function() {};
     _.extend($, $.__views);
     $.win.navBarHidden = true;
     $.win.open();
-    __defers["$.__views.__alloyId206!click!openPhoneNumberWin"] && $.__views.__alloyId206.addEventListener("click", openPhoneNumberWin);
+    __defers["$.__views.__alloyId208!click!openPhoneNumberWin"] && $.__views.__alloyId208.addEventListener("click", openPhoneNumberWin);
     _.extend($, exports);
 }
 
